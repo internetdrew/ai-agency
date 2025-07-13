e9b1700497dd08e037b33a63a662013dfd9b6293
Andrew Rowley <andrew@internetdrew.com>
2025-07-13 02:06:48 -0400

    add initial persona route

--- src/routes/Personas.tsx
+++ src/routes/Personas.tsx
@@ -1,5 +1,31 @@
+import CreatePersonaDialog from "@/components/CreatePersonaDialog";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { useState } from "react";
+
 const Personas = () => {
-  return <div className="p-4 pt-0">Personas</div>;
+  const [renderPersonaDialog, setRenderPersonaDialog] = useState(false);
+
+  return (
+    <>
+      <div className="p-4 pt-0">
+        <div className="flex items-center justify-between">
+          <h1 className="mb-4 text-2xl font-bold">Personas</h1>
+          <Button
+            variant="outline"
+            onClick={() => setRenderPersonaDialog(true)}
+          >
+            <PlusIcon className="h-4 w-4" />
+            Add Persona
+          </Button>
+        </div>
+      </div>
+      <CreatePersonaDialog
+        open={renderPersonaDialog}
+        onOpenChange={setRenderPersonaDialog}
+      />
+    </>
+  );
 };
 
 export default Personas;
